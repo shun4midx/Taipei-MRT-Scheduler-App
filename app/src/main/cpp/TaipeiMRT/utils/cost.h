6f7ed80232d0c3e4eaad306e58cb3675faccdc89
shun4midx <shun4midx@gmail.com>
2026-09-08 18:40:19 +0800

    Rename my old name

--- app/src/main/cpp/TaipeiMRT/utils/cost.h
+++ app/src/main/cpp/TaipeiMRT/utils/cost.h
@@ -1,5 +1,5 @@
 /********************************************
- * Copyright (c) 2026 Shun/翔海 (@shun4midx) *
+ * Copyright (c) 2026 Shun/修海 (@shun4midx) *
  * Project: Taipei-MRT-Scheduler            *
  * File Type: C++ Header file               *
  * File: cost.h                             *
